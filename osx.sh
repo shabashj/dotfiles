c0187bbc0a493c1a5ab3277352a32a954538388d
Evgeni Shabash <evgeni.s@taboola.com>
2020-12-18 12:45:22 -0800

    add homebrew to mac script

--- osx.sh
+++ osx.sh
@@ -1,1 +1,25 @@
-cp .vimrc .zshrc $HOME
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update homebrew recipes
+brew update
+
+PACKAGES=(
+    git
+    python
+    python3
+    neovim
+    wget
+    the_silver_searcher
+    ripgrep
+    grv
+)
+
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+
+echo "Clone configs..."
+git clone https://github.com/shabashj/dotfiles.git ~/
